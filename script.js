d963746279614e10ce5ff4f48ad6b0a2a9900157
Francisvan <francisvan.oliveira@francisvanoliveira.com.br>
2023-01-18 00:16:36 -0400

    Project styling

---
+++ script.js
@@ -0,0 +1,31 @@
+const inputEl = document.querySelector("#password")
+
+let passwordLength = 16
+
+function generatePassword(){
+    const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
+
+    let password = ""
+
+    for(let i = 0; i < passwordLength; i++){
+        const randomNumber = Math.floor(Math.random() * chars.length)
+        password += chars.substring(randomNumber, randomNumber +1)
+    }
+
+    inputEl.value = password
+}
+
+function copy() {
+    navigator.clipboard.writeText(inputEl.value)
+}
+
+const passwordLengthEl = document.querySelector("#password-lenght")
+passwordLengthEl.addEventListener("input", function(){
+    passwordLength = passwordLengthEl.value
+    generatePassword()
+})
+
+document.querySelector("#copy-1").addEventListener("click", copy)
+document.querySelector("#copy-2").addEventListener("click", copy)
+
+generatePassword()
